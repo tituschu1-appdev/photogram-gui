fc65de2a8a4e6183a46f4e2b9e02323b7c90df23
Titus Chu <tituschu@uchicago.edu>
2023-05-01 02:11:57 +0000

    adding photos works

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -33,4 +33,22 @@ class PhotosController < ApplicationController
 
     redirect_to("/photos")
   end
+
+  def create
+    # Parameters: {"query_image"=>"", "query_caption"=>"", "query_owner_id"=>""}
+    input_image = params.fetch("query_image")
+    input_caption= params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    a_new_photo = Photo.new
+
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save
+
+    #render({ :template => "photo_templates/create.html.erb"})
+    redirect_to("/photos/" + a_new_photo.id.to_s)
+  end
 end

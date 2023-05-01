c738fd7399596c93ed14796402e495ab9e16b94a
Titus Chu <tituschu@uchicago.edu>
2023-05-01 00:07:35 +0000

    2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,20 @@ class UsersController < ApplicationController
     @list_of_users = matching_users.order({ :username => :asc})
 
     render({ :template => "user_templates/index.html.erb"})
+  end
 
+  def show
+    # parasms {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username})
+
+    @the_user = matching_usernames.at(0)
+
+    if @the_user == nil
+      redirect_to("/404")
+    else
+      render({ :template => "user_templates/show.html.erb"})
+    end
   end
 end
